3c14cdaee21fc19f8cd41a56a4ae1578be23fb06
unco <uncopia@gmail.com>
2025-08-06 16:29:23 -0400

    md

--- init.lua
+++ init.lua
@@ -101,6 +101,23 @@ require("lualine").setup({ options = { theme = "gruvbox" } })
 
 -- nvim-tree file explorer
 require("nvim-tree").setup({
+  on_attach = function(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open file/folder"))
+
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Collapse/Up"))
+  end,
   hijack_cursor = true,
   view = { width = 30, side = "left" },
   renderer = { icons = { show = { folder = true, file = true } } },
