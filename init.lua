1e4c9e0e9c6aee3efe155be4054340fb6fa01347
unco <uncopia@gmail.com>
2025-08-06 18:40:22 -0400

    md

--- init.lua
+++ init.lua
@@ -1,80 +1,50 @@
--- init.lua configuration file for Neovim
---
--- This configuration bootstraps a modern Neovim setup aimed at
--- JavaScript/TypeScript development.  It uses the community‑endorsed
--- plugin manager “lazy.nvim” to manage all plugins.  Language
--- servers are installed via “mason.nvim” and configured with the
--- built‑in LSP client introduced in Neovim 0.11+.  Code formatting
--- is provided by “conform.nvim”.  Autocompletion uses
--- “blink.cmp”, a fast completion engine with sensible defaults
--- that integrates with the LSP for suggestions【894503498548323†L96-L110】【962288523264436†L69-L84】.
-
 --//////////////////////////////////////////////////////////////////////////////
+-- Neovim init.lua tailored for JavaScript/TypeScript (Screeps) development
+--//////////////////////////////////////////////////////////////////////////////
+
 -- Basic options
-vim.opt.hlsearch       = true          -- highlight search matches
-vim.opt.number         = true          -- show absolute line numbers
-vim.opt.relativenumber = true          -- relative line numbers
-vim.opt.mouse          = "a"           -- enable mouse support
-vim.opt.showmode       = false         -- don’t display mode (handled by lualine)
-vim.opt.termguicolors  = true          -- enable 24‑bit colours
-vim.opt.wrap           = false         -- don’t wrap long lines
-vim.opt.tabstop        = 2             -- tabs are 2 spaces
-vim.opt.shiftwidth     = 2             -- indentation is 2 spaces
-vim.opt.expandtab      = true          -- expand tabs to spaces
-vim.opt.smartcase      = true          -- smart case search
-vim.opt.ignorecase     = true          -- ignore case in searches unless uppercase used
-vim.opt.splitright     = true          -- open vertical splits to the right
-vim.opt.splitbelow     = true          -- open horizontal splits below
-vim.opt.clipboard       = "unnamedplus"
+vim.opt.hlsearch = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.clipboard = "unnamedplus"
 
--- Use a comma as the <leader> key.  Many of the commands below are bound
--- using this prefix.  See the “Key bindings” section in the reference
--- article for rationale【894503498548323†L286-L294】.
 vim.g.mapleader = " "
-
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 --//////////////////////////////////////////////////////////////////////////////
--- Plugin specification
+-- Plugins
 local plugins = {
-  { "nvim-lua/plenary.nvim" },       -- utility functions used by many plugins
-  { "nvim-tree/nvim-web-devicons" }, -- file icons
-
-  -- Theme.  Feel free to replace "gruvbox" with your preferred theme.
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
   { "ellisonleao/gruvbox.nvim" },
-
-  -- Status line
   { "nvim-lualine/lualine.nvim" },
-
-  -- File explorer tree
   { "nvim-tree/nvim-tree.lua" },
-
-  -- Fuzzy finder and command palette
   { "nvim-telescope/telescope.nvim" },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-
-  -- Syntax highlighting and incremental selection
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-  -- LSP installer and configuration
   { "williamboman/mason.nvim" },
   { "neovim/nvim-lspconfig" },
   { "mason-org/mason-lspconfig.nvim" },
-
-  -- Formatter/linter bridge.  This plugin will use external CLI tools
-  -- (such as Prettier) for languages where the LSP doesn’t provide
-  -- formatting【894503498548323†L241-L259】.
   { "stevearc/conform.nvim" },
-
-  -- Completion engine.  blink.cmp works out‑of‑the‑box and includes
-  -- built‑in sources for LSP, buffer, path and snippets【962288523264436†L69-L115】.
   {
     "saghen/blink.cmp",
     version = "1.*",
-    opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" },
   },
 }
 
--- Bootstrap lazy.nvim (plugin manager).  This clones the plugin on
--- first run and prepends it to the runtime path【894503498548323†L148-L160】.
+-- Plugin manager bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -93,124 +63,144 @@ require("lazy").setup(plugins)
 -- Plugin configuration
 
 -- Theme
-vim.o.background = "dark" -- set "light" for light backgrounds
+vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
--- Lualine (status line)
+-- Lualine
 require("lualine").setup({ options = { theme = "gruvbox" } })
 
--- nvim-tree file explorer
+-- nvim-tree
 require("nvim-tree").setup({
   on_attach = function(bufnr)
     local api = require("nvim-tree.api")
+    -- load all default mappings first
+    api.config.mappings.default_on_attach(bufnr)
 
+    -- now override only the keys you want to change
     local function opts(desc)
-      return {
-        desc = "nvim-tree: " .. desc,
-        buffer = bufnr,
-        noremap = true,
-        silent = true,
-        nowait = true,
-      }
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
-
-    vim.keymap.set("n", "l", api.node.open.edit, opts("Open file/folder"))
-
-    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Collapse/Up"))
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))      -- open file/folder
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Parent"))
   end,
   hijack_cursor = true,
   view = { width = 30, side = "left" },
   renderer = { icons = { show = { folder = true, file = true } } },
 })
 
--- Telescope fuzzy finder
+-- Telescope
 local telescope = require("telescope")
 telescope.setup({})
 telescope.load_extension("fzf")
 
--- Treesitter: ensure JavaScript/TypeScript parsers are installed and enable
--- highlighting and indentation
+-- Treesitter
 require("nvim-treesitter.configs").setup({
-  ensure_installed = {
-    "lua",
-    "javascript",
-    "typescript",
-    "json",
-    "html",
-    "css",
-  },
+  ensure_installed = { "lua", "javascript", "typescript", "json", "html", "css" },
   highlight = { enable = true },
   indent = { enable = true },
 })
 
--- Mason: tool manager for LSP servers and formatters
+--//////////////////////////////////////////////////////////////////////////////
+-- LSP setup
+
+-- Mason
 require("mason").setup()
 
--- Mason‑lspconfig: ensure required LSP servers are installed automatically
+-- Mason-lspconfig
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "eslint",   -- ESLint server for linting and some formatting
+    "eslint",
+    "ts_ls",
   },
+  automatic_installation = true,
+  automatic_setup = true,
+  automatic_enable = { exclude = { "ts_ls" } },
 })
 
--- Setup LSP servers.  Starting from Neovim 0.11 you need to enable each
--- server explicitly; mason-lspconfig will download binaries for you.
--- See the nvim-lspconfig quickstart for details【129280182321571†L356-L369】.
-local lsp = vim.lsp
-lsp.enable("eslint")
+-- LSPConfig
+local lspconfig = require("lspconfig")
 
--- Configure inlay hints globally.  Inlay hints show parameter names and
--- inferred types inline【894503498548323†L203-L216】.
+-- ESLint
+lspconfig.eslint.setup({})
+
+-- TypeScript/JavaScript (with Screeps typings support)
+local lspconfig = require("lspconfig")
+lspconfig.ts_ls.setup({
+  root_dir = require("lspconfig.util").root_pattern("tsconfig.json", "package.json", ".git"),
+  single_file_support = true,
+  on_attach = function(client, bufnr)
+    -- example keymaps
+    vim.lsp.buf.hover(bufnr)
+  end,
+})
+
+-- Inlay hints
 vim.lsp.inlay_hint.enable(true)
 
--- Setup conform.nvim for formatting.  Prettier will be used for JS/TS/JSON
--- files; it falls back to the LSP’s formatting if no formatter is
--- configured【894503498548323†L241-L259】.
+--//////////////////////////////////////////////////////////////////////////////
+-- Formatter: conform.nvim (Prettier fallback)
 require("conform").setup({
   default_format_opts = { lsp_format = "fallback" },
   formatters_by_ft = {
-    javascript       = { "prettier" },
-    javascriptreact  = { "prettier" },
-    typescript       = { "prettier" },
-    typescriptreact  = { "prettier" },
-    json             = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    json = { "prettier" },
   },
 })
 
--- blink.cmp configuration.  Default settings provide LSP, buffer, path
--- and snippet completion with typo‑resistant fuzzy matching【962288523264436†L69-L115】.
+--//////////////////////////////////////////////////////////////////////////////
+-- Completion: blink.cmp
 require("blink.cmp").setup({})
 
 --//////////////////////////////////////////////////////////////////////////////
--- Key bindings
-vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<C-c>", ":q<CR>", { desc = "Quit window" })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Focus left window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Focus right window" })
-
+-- Keybindings
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "x", '"_x') -- delete character without yanking
+vim.keymap.set("n", "d", '"_d') -- delete without yanking
+vim.keymap.set("v", "d", '"_d') -- visual delete without yanking
+vim.keymap.set("n", "c", '"_c') -- change without yanking
+vim.keymap.set("v", "c", '"_c') -- visual change without yanking
+-- Saving, quitting
+map("n", "<C-s>", ":w<CR>", { desc = "Save file" })
+map("n", "<C-c>", ":q<CR>", { desc = "Quit window" })
 
--- Quick access to nvim-tree
-map("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggles the tree open/closed
-map("n", "<C-f>", ":NvimTreeFindFile<CR>") -- find current file in the tree
--- map("n", "<C-c>", ":NvimTreeClose<CR>")    -- close the tree
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Focus left window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Focus right window" })
 
--- Format the current buffer using conform.nvim
-map("n", "<leader>fo", function() require("conform").format() end)
+-- nvim-tree
+map("n", "<leader>e", ":NvimTreeToggle<CR>")
+map("n", "<C-f>", ":NvimTreeFindFile<CR>")
 
--- Telescope keybindings for quick file and symbol search
+-- Formatting
+map("n", "<leader>fo", function() require("conform").format() end, { desc = "Format file" })
+map("n", "<leader>f", function()
+  require("conform").format({ async = true, lsp_fallback = true })
+end, { desc = "Format with Prettier" })
+-- Telescope
 map("n", "<leader>p", require("telescope.builtin").find_files)
 map("n", "<leader>fg", require("telescope.builtin").live_grep)
 map("n", "<leader>fb", require("telescope.builtin").buffers)
 map("n", "<leader>fh", require("telescope.builtin").help_tags)
 
--- LSP navigation shortcuts (jump to definition, hover, diagnostics, etc.)
-map("n", "gd", vim.lsp.buf.definition)
-map("n", "K", vim.lsp.buf.hover)
-map("n", "gr", vim.lsp.buf.references)
-map("n", "<leader>rn", vim.lsp.buf.rename)
-map("n", "<leader>ca", vim.lsp.buf.code_action)
-map("n", "[d", vim.diagnostic.goto_prev)
-map("n", "]d", vim.diagnostic.goto_next)
+-- LSP keymaps
+map("n", "K", vim.lsp.buf.hover, opts)
+map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+map("n", "gd", vim.lsp.buf.definition, opts)
+map("n", "gD", vim.lsp.buf.declaration, opts)
+map("n", "gi", vim.lsp.buf.implementation, opts)
+map("n", "gr", vim.lsp.buf.references, opts)
+map("n", "gt", vim.lsp.buf.type_definition, opts)
+map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+map("n", "[d", vim.diagnostic.goto_prev, opts)
+map("n", "]d", vim.diagnostic.goto_next, opts)
+map("n", "<leader>E", vim.diagnostic.open_float, opts)
+map("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, opts)
+map("n", "<leader>ws", require("telescope.builtin").lsp_workspace_symbols, opts)
+map("n", "<leader>ws", require("telescope.builtin").lsp_workspace_symbols, opts)
 
--- End of init.lua
+
